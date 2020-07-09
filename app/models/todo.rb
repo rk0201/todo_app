4dc72f65d676bb83ee9e01c988f076f0d6de42bb
EC2 Default User <ec2-user@ip-172-31-28-109.us-east-2.compute.internal>
2020-07-09 04:29:13 +0000

    finish 2-15

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,10 +1,14 @@
 class Todo < ApplicationRecord
   belongs_to :goal
-   acts_as_list scope: [:goal_id]
+   has_many :todo_tags
+   has_many :tags, through: :todo_tags
+   accepts_nested_attributes_for :todo_tags
 
-  validates :position, numericality: { only_integer: true, allow_nil: true }
-  validates :content, presence: true
-  validates :done, inclusion: { in: [true, false] }
+    acts_as_list scope: [:goal_id]
+
+    validates :position, numericality: { only_integer: true, allow_nil: true }
+    validates :content, presence: true
+    validates :done, inclusion: { in: [true, false] }
  
   after_initialize :set_default, if: :new_record?
  
